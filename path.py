14970aa89ea3f2574e9a6dcb61c3f6a4b0770534
Hargent <94790646+Hargent@users.noreply.github.com>
2022-01-13 16:14:35 +0100

    Add initial cv website files

---
+++ path.py
@@ -0,0 +1,20 @@
+import os
+from PROJECT.personal.Image_Editor import _rename_
+import pathlib
+pathlib.Path.anchor.join()
+pathlib.WindowsPath.as_posix(
+    'C:\\Users\\SILVER\\.vscode\\PROJECT\\scripts\\web_server\\script.js')
+# print(pathlib.PosixPath.as_posix(
+# 'C:\\Users\\SILVER\\.vscode\\PROJECT\\scripts\\web_server\\script.js'))
+# def path(win_path):
+#     # take windows path input
+#     # convertslashes from backward to foward
+
+#     pathlib.PosixPath.as_posix(win_path)
+#     #os.replace(j, '/')
+#     # return a string of path with foward slashes
+#     return win_path
+#     # pass
+_rename_()
+
+# print(path(Scripts))
